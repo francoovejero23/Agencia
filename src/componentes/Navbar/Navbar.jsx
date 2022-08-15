62aae749539ded899aac23b1cc15019aa9ba7f1c
wilbernp <wilbernarvaezpetro@gmail.com>
2022-08-14 21:01:18 -0500

    agregando funcionalidad a la navBar

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-scroll';
 import logo from '../../images/Multid.png';
 import multi from '../../images/Mesa de trabajo 2 copia.png'
+import "./navBar.css"
+
+
+
+
+// className={` fixed-top  ${
+//   !isTop ? "navbar-white" : "navbar-transparent"
+// }`}
 
 function Navbar() {
+  let [prevScroll, setPrevScroll] = useState(0)
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=>{
+        setPrevScroll(window.scrollY)
+    })
+},[prevScroll])
+
+
   return (
     <div>
-      <nav class="padre">
+      <nav className={`padre ${prevScroll>0?"color":"transparent"}`}>
       <Link to='main' className='logo'>
         <img src={logo} alt='logo'/>
         </Link>
