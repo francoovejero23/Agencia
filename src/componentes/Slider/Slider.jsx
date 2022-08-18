4659e6ee1831107674c0cc08cdab6fa2cb1ce6b5
wilbernp <wilbernarvaezpetro@gmail.com>
2022-08-17 22:56:00 -0500

    agregando dots

--- src/componentes/Slider/Slider.jsx
+++ src/componentes/Slider/Slider.jsx
@@ -6,17 +6,13 @@ import Card from './Card'
 export default function Slider() {
     let [current, setCurrent] = useState(0)
     let currentSlider=integrantes[current]
-
-
     function next(){
         if (current>=integrantes.length-1) {
             setCurrent(0)
         } else{
             setCurrent(current + 1)
         }
-
     }
-
     function prev(){
         if (current<=0) {
             setCurrent(integrantes.length-1)
@@ -24,22 +20,16 @@ export default function Slider() {
             setCurrent(current - 1)
         }
     }
-    // function handleClick(value) {
-    //     if (value<0) {
-    //         setCurrentSlider(integrantes[integrantes.length-1])
-    //         setCurrent(integrantes.length)
 
-    //     } else if (value >= integrantes.length) {
-    //         setCurrentSlider(0)
-    //         setCurrent(0)
-    //     } else{
-    //         setCurrentSlider(integrantes[value])
-    //         setCurrent(value)
+    function handleClick(index){
 
-    //     }
-       
-    // }
-
+    if (index > current) {
+        next()
+    } else if (index < current) {
+        prev()
+    }
+        
+    }
     return (
         <div id="default-carousel" className="relative" data-carousel="static">
             <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
@@ -47,22 +37,20 @@ export default function Slider() {
                     {
                         <Card {...currentSlider}/>
                     }
-
-                    {/* <img src={avatar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" /><img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."> */}
                 </div>
-                {/* <div className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10" data-carousel-item="">
-                    <img src={avatar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-
-                </div>
-                <div className="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
-
-                    <img src={avatar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-                </div> */}
             </div>
+            
+           
             <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                <button type="button" className="w-3 h-3 rounded-full bg-red-700 dark:bg-gray-800" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" className="w-3 h-3 rounded-full bg-blue-600 dark:bg-gray-800/50 hover:bg-black dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" className="w-3 h-3 rounded-full bg-blue-600 dark:bg-gray-800/50 hover:bg-black dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                {
+                    integrantes.map((e , index)=>{
+                        return(
+                            <button onClick={()=>handleClick(index)} type="button" className={`${index === current?"bg-red-700":"bg-blue-600" } w-3 h-3 rounded-full  dark:bg-gray-800" aria-current="true" aria-label="Slide 1`} data-carousel-slide-to="0"></button>
+                        )
+                    })
+                }
+                {/* <button type="button" className="w-3 h-3 rounded-full bg-blue-600 dark:bg-gray-800/50 hover:bg-black dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                <button type="button" className="w-3 h-3 rounded-full bg-blue-600 dark:bg-gray-800/50 hover:bg-black dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button> */}
             </div>
             <button onClick={() => prev()} type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
                 <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-gray-500 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-blue-700 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
