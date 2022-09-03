00433dfdcfee4312072e0df35aca2af71c9cd7d1
wilbernp <wilbernarvaezpetro@gmail.com>
2022-09-03 16:15:29 -0500

    corrigiendo el slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Slider from './componentes/Slider/Slider.jsx';
+// import Slider from './componentes/Slider/Slider.jsx';
 import Header from './componentes/Header/Header.jsx';
 import Metodo from './componentes/Metodo/Metodo.jsx'
 import Presupuestos from './componentes/Presupuestos/Presupuestos.jsx';
 import Servicios from './componentes/Servicios/Servicios.jsx';
+import Slider from './componentes/Slider/Slider.jsx';
 // import Slider from './componentes/Slider/Slider.jsx';
 // import Presupuestos from './components/Presupuestos.jsx';
 
