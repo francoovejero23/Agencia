02e12d606efb7da07f2c84a7250857f581b02fc4
wilbernp <wilbernarvaezpetro@gmail.com>
2022-09-06 18:36:21 -0500

    corrigiendo errores en presupuestos

--- src/componentes/Presupuestos/Presupuestos.jsx
+++ src/componentes/Presupuestos/Presupuestos.jsx
@@ -1,36 +1,78 @@
-import React from 'react'
-import s from "./presupuestos.module.css"
-import './presupuestos.module.css'
-import image from '../../images/charlesdeluvio-Lks7vei-eAg-unsplash.jpg'
+
+import "./presupuestos.css"
 import image1 from '../../images/alessio-zaccaria-MplUOXqEUK0-unsplash.jpg'
 import image2 from '../../images/austin-distel-wD1LRb9OeEo-unsplash.jpg'
+import image3 from '../../images/charlesdeluvio-Lks7vei-eAg-unsplash.jpg'
+
+
+
 export default function Presupuestos() {
     return (
-        <div className={s["grid-container"]}>
+        <div className="grid_container-pres">
             <div>
-                <h1 className={s.presu}>Presupuestos<br></br>Personalizados</h1>
-                
-                <h3 className={s.parrafo}>
+                <h1 className={"presu"}>Presupuestos<br></br>Personalizados</h1>
+
+                <h3 className={"parrafo"}>
                     Si te gustaria contratar alguno de nuestros servicios recibir mas informacion al respecto
                     contactanos para una asesoria gratuita con nuestro equipo
                 </h3>
-                <button className={s.buttons}>MAS INFO</button>
+                <button className={"buttons"}>MAS INFO</button>
             </div>
-            <div className={s["grid-container"]}>
-                <div className={s.sub}>
-                <img className={s.tradin2} src={image1} alt=""/>
-                </div>
-
-                <div className={s.sub}>
-                <img className={s.tradin1} src={image2} alt=""/>
-                </div>
-
-                <div className={s.image3}>
-                <img src={image} alt=""/>
-                </div>
+            <div className="grid_container-pres-img">
+                <img className="image_pres" src={image1} alt="" />
+                <img className="image_pres" src={image2} alt="" />
+                <img className="image_pres image_pres-large" src={image3} alt="" />
             </div>
         </div>
-
-       
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function Presupuestos() {
+//     return (
+//         <div className={s["grid-container"]}>
+//             <div>
+//                 <h1 className={s.presu}>Presupuestos<br></br>Personalizados</h1>
+
+//                 <h3 className={s.parrafo}>
+//                     Si te gustaria contratar alguno de nuestros servicios recibir mas informacion al respecto
+//                     contactanos para una asesoria gratuita con nuestro equipo
+//                 </h3>
+//                 <button className={s.buttons}>MAS INFO</button>
+//             </div>
+//             <div className={s["grid-container"]}>
+//                 <div className={s.sub}>
+//                 <img className={s.tradin2} src={image1} alt=""/>
+//                 </div>
+
+//                 <div className={s.sub}>
+//                 <img className={s.tradin1} src={image2} alt=""/>
+//                 </div>
+
+//                 <div className={s.image3}>
+//                 <img src={image} alt=""/>
+//                 </div>
+//             </div>
+//         </div>
+
+
+//     )
+// }
+
+
+
